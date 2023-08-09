c2041dbc9c5048f26638558bf48775f7b96cdd39
ericodwr <ericodr@upnvj.ac.id>
2023-08-09 14:02:53 +0700

    updating whole projects

--- projects/candidate/src/app/app.routing.ts
+++ projects/candidate/src/app/app.routing.ts
@@ -1,67 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BaseModule } from './components/base/base.module';
-import { BaseComponent } from './components/base/base.component';
+import { BaseModule } from '@components/base/base.module';
+import { BaseComponent } from '@components/base/base.component';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
+import { NotFoundComponent } from '@components/not-found/not-found.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { roleValidation } from './validation/role.validation';
+// import { roleValidation } from './validation/role.validation';
 import { Roles } from './constant/role.constant';
-import {
-  authNonLoginValidation,
-  authValidation,
-} from './validation/auth.validation';
+// import {
+//   authNonLoginValidation,
+//   authValidation,
+// } from './validation/auth.validation';
 import { InputTextModule } from 'primeng/inputtext';
-import { ButtonComponent } from './components/button/button.component';
+import { ButtonComponent } from '@components/button/button.component';
 
 const routes: Routes = [
-  {
-    path: 'users',
-    component: BaseComponent,
-    loadChildren: () =>
-      import('./pages/users/user.module').then((u) => u.UserModule),
-    canMatch: [authNonLoginValidation],
-  },
-  {
-    path: 'questions',
-    component: BaseComponent,
-    loadChildren: () =>
-      import('./pages/questions/questions.module').then(
-        (u) => u.QuestionModules
-      ),
-    canMatch: [authNonLoginValidation],
-  },
-  {
-    path: 'assign-candidate',
-    component: BaseComponent,
-    loadChildren: () =>
-      import('./pages/assign-candidates/assign-candidate.module').then(
-        (u) => u.AssignCandidateModule
-      ),
-    canMatch: [authNonLoginValidation],
-  },
-  {
-    path: 'candidates',
-    loadChildren: () =>
-      import('./pages/candidates/candidates.module').then(
-        (u) => u.CandidatesModules
-      ),
-    canMatch: [authNonLoginValidation],
-  },
-  {
-    path: 'reviews',
-    component: BaseComponent,
-    loadChildren: () =>
-      import('./pages/review/review.module').then((u) => u.ReviewModule),
-    data: [Roles.REVIEWER],
-    canMatch: [authNonLoginValidation, roleValidation],
-  },
+  // {
+  //   path: 'users',
+  //   component: BaseComponent,
+  //   loadChildren: () =>
+  //     import('./pages/users/user.module').then((u) => u.UserModule),
+  //   canMatch: [authNonLoginValidation],
+  // },
+
   {
     path: 'login',
     component: LoginComponent,
-    canMatch: [authValidation],
+    // canMatch: [authValidation],
   },
   {
     path: 'dashboard',
@@ -72,7 +39,7 @@ const routes: Routes = [
         component: DashboardComponent,
       },
     ],
-    canMatch: [authNonLoginValidation],
+    // canMatch: [authNonLoginValidation],
   },
   {
     path: '',
--- projects/candidate/src/app/pages/dashboard/dashboard.component.ts
+++ projects/candidate/src/app/pages/dashboard/dashboard.component.ts
@@ -1,19 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../services/auth.service';
+// import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
   fullName: string = '';
 
-  constructor(private authService: AuthService) {}
+  // constructor(private authService: AuthService) {}
 
-  ngOnInit(): void {
-    const profile = this.authService.getProfile();
-    if (profile) {
-      this.fullName = profile.fullName;
-    }
-  }
+  // ngOnInit(): void {
+  //   const profile = this.authService.getProfile();
+  //   if (profile) {
+  //     this.fullName = profile.fullName;
+  //   }
+  // }
 }
