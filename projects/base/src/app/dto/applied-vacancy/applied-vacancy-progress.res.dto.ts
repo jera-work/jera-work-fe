f17d32d0a0e4b480220108c250675e773767c55d
Josep Victor <thejosepvictorr@gmail.com>
2023-08-29 09:00:15 +0700

    fix job card, applied job detail, applied job list

--- projects/base/src/app/dto/applied-vacancy/applied-vacancy-progress.res.dto.ts
+++ projects/base/src/app/dto/applied-vacancy/applied-vacancy-progress.res.dto.ts
@@ -1,3 +1,4 @@
 export interface AppliedVacancyProgressResDto {
+	jobVacancyId: string
 	progressCode: string
 }
--- projects/candidate/src/app/pages/applied-job/applied-list/applied-job-list.component.ts
+++ projects/candidate/src/app/pages/applied-job/applied-list/applied-job-list.component.ts
@@ -2,11 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { AppliedVacancyResDto } from '@dto/applied-vacancy/applied-vacancy.res.dto';
-import { JobSearchResDto } from '@dto/job-vacancy/job-search.res.dto';
-import { JobVacancyResDto } from '@dto/job-vacancy/job-vacancy.res.dto';
 import { AppliedVacancyService } from '@services/applied-vacancy.service';
-import { JobVacancyService } from '@services/job-vacancy.service';
 import { firstValueFrom } from 'rxjs';
+import { ProgressCode } from '../../../constant/progress.constant';
+import { StatusCode } from '../../../constant/status.constant';
 
 @Component({
   selector: 'applied-job-list',
@@ -50,11 +49,27 @@ export class AppliedJobListComponent implements OnInit {
     this.router.navigateByUrl(`/applied-job/${appliedId}`)
   }
 
-  getSeverity(status: string): string {
-    if (status === 'Application') {
-      return 'success';
+  getProgressColor(progress: string): string {
+    if (progress === ProgressCode.APP) {
+      return 'linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%)';
+    } else if (progress === ProgressCode.ASS|| progress === ProgressCode.INT){
+      return 'linear-gradient(-225deg,#AC32E4 0%,#7918F2 48%,#4801FF 100%)';
+    } else if (progress === ProgressCode.MCU){
+      return 'linear-gradient(160deg, #0093E9 0%, #80D0C7 100%)';
+    } else if (progress === ProgressCode.OFL){      
+      return 'linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)';
     } else {
-      return 'danger';
+      return 'linear-gradient(45deg, #85FFBD 0%, #FFFB7D 100%)';
+    }
+  }
+  
+  getStatusColor(status: string): string {
+    if(status === StatusCode.ACT) {
+      return 'linear-gradient(212deg, #10B940 0%, #ede947 100%)'
+    } else if (status === StatusCode.CLS) {
+      return 'linear-gradient(90deg, #272724 0%, #4a4a4a 100%)'
+    } else {
+      return 'linear-gradient(135deg, #0c32bb 14%, #FF0000 100%)'
     }
   }
 }
